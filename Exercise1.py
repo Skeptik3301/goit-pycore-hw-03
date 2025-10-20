234621024b8355134c8bbb02777c0d7d9024756a
Skeptik3301 <ilnitskiynik@gmail.com>
2025-10-20 13:43:19 +0300

    Implement error handling in get_days_from_today
    Added error handling for date parsing.

--- Exercise1.py
+++ Exercise1.py
@@ -1,7 +1,13 @@
 from datetime import datetime  
+
 def get_days_from_today(date):
-    date_obj = datetime.strptime(date, "%Y-%m-%d").date()
-    today = datetime.today().date()
-    delta = today - date_obj
-    return delta.days
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        delta = today - date_obj
+        return delta.days
+    except ValueError:
+        print("Помилка: неправильний формат дати. Використовуйте формат YYYY-MM-DD.")
+        return None
+
 print(get_days_from_today("2025-10-20"))
