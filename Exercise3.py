8770033c9af6ed41894ca770005b9bc61cf3c64a
Skeptik3301 <ilnitskiynik@gmail.com>
2025-10-20 13:44:08 +0300

    Refactor normalize_phone function for better handling
    Refactor phone number normalization logic to handle various formats and whitespace. Update raw numbers list to include additional test cases.

--- Exercise3.py
+++ Exercise3.py
@@ -1,25 +1,32 @@
 def normalize_phone(phone_number):
-    clean_number = ""  
-    for ch in phone_number:  
-        if ch.isdigit() or ch == '+':  
+    phone_number = phone_number.strip()
+    clean_number = ""
+
+    for i, ch in enumerate(phone_number):
+        if ch.isdigit() or (ch == '+' and i == 0):
             clean_number += ch
-    if clean_number.startswith('+380'):
+
+
+    if clean_number.startswith('+'):
         return clean_number
-    elif clean_number.startswith('380'):
-        return '+' + clean_number
-    elif clean_number.startswith('0'):
+
+    elif clean_number.startswith('0') or clean_number.startswith('3') or clean_number.isdigit():
         return '+38' + clean_number
+
+    else:
+        return None
+
+
 raw_numbers = [
-    "067\t123 4567",
-    "(095) 234-5678\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
+    "067\t123 4567",          
+    "(095) 234-5678\n",       
+    "+380 44 123 4567",       
+    "501234567",           
+    "+1 (202) 555-0147",      
+    "   +48 123 456 789",     
+    "0503451234",             
+    "+49 30 901820",         
 ]
 
-sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers if normalize_phone(num) is not None]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
